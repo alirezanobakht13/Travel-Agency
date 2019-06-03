a0b7ffdb4c71ba596936008b229a018efa5b55ce
Alireza Nobakht <51335235+alirezanobakht13@users.noreply.github.com>
2019-06-03 11:52:18 -0700

    uploading files
    these files were on my other Github accounts and I bring them here.

---
+++ Travel_Agency-master/Admin/admin.h
@@ -0,0 +1,11 @@
+//
+// Created by mgh on 12/24/18.
+//
+
+#ifndef TRAVEL_AGENCY_ADMIN_H
+#define TRAVEL_AGENCY_ADMIN_H
+
+//Main function::::
+int Admin();
+
+#endif //TRAVEL_AGENCY_ADMIN_H
---
+++ Travel_Agency-master/Drivers/drivers.h
@@ -0,0 +1,8 @@
+//
+// Created by mgh on 12/24/18.
+//
+
+#ifndef TRAVEL_AGENCY_DRIVERS_H
+#define TRAVEL_AGENCY_DRIVERS_H
+
+#endif //TRAVEL_AGENCY_DRIVERS_H
